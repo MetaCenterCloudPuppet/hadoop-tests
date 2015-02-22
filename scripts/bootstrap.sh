8936decc5a4dab9637961399069147d82944648b
František Dvořák <valtri@civ.zcu.cz>
2015-02-22 22:22:33 +0100

    Bootstrap scripts: update ubuntu, add RHEL 6.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,10 +4,12 @@ if grep -q '^Debian' /etc/issue; then
   `dirname $0`/debian-puppet.sh
 elif grep -q '^Ubuntu' /etc/issue; then
   `dirname $0`/debian-update.sh
-  `dirname $0`/debian-puppet.sh
+  `dirname $0`/ubuntu-puppet.sh
 elif grep -q '^Fedora' /etc/issue; then
   `dirname $0`/fedora-update.sh
   `dirname $0`/fedora-puppet.sh
 elif grep -q '^\(RedHat\|CentOS\)' /etc/issue; then
+  major=`sed -e 's/.*release \([0-9]\+\).*/\1/' /etc/redhat-release`
+  `dirname $0`/rhel6-puppet.sh
   `dirname $0`/redhat-update.sh
 fi
