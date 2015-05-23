2cb228fecf63a770f1eb551bd6271244331cd6a7
František Dvořák <valtri@civ.zcu.cz>
2015-05-23 10:03:56 +0200

    Not installing virtualbox guest additions in Fedora.

--- scripts/fedora-update.sh
+++ scripts/fedora-update.sh
@@ -1,13 +1,5 @@
 #! /bin/bash -xe
 
-if test ! -f /etc/yum.repos.d/rpmfusion-nonfree.repo; then
-  wget -nv http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-21.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-21.noarch.rpm
-  yum install -y ./rpmfusion-*.rpm
-  yum remove -y kmod-VirtualBox\*
-  yum upgrade -y
-  yum install -y VirtualBox-guest || :
-fi
-
 if test -f /lib/systemd/system/firewalld.service; then
   service firewalld stop || :
   yum remove -y firewalld
@@ -18,7 +10,6 @@ if test -f /lib/systemd/system/firewalld.service; then
 :OUTPUT ACCEPT [0:0]
 COMMIT
 EOF
-
 fi
 
 yum upgrade -y
